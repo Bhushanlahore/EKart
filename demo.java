619838434e3baa626e20b99a4bf420f37186a85a
Bhushan <bhushanlahore03@gmail.com>
2022-12-13 17:05:25 +0530

    reverse string added

--- demo.java
+++ demo.java
@@ -13,6 +13,21 @@ class demo{
 		
 		System.out.println(rev);
 	}
+
+//bug no 101 fixing changes
+public static void reverseStr(String str){
+	String str1 = "";
+	int n = str.length();
+
+	for(int i=n-1; i>=0; i--){
+		
+		str1 = str1 + str.charAt(i);
+	}
+
+	System.out.print(str1);
+}
+
+
 public static void main(String args[]){
 
 System.out.println("HEELO WORLD");
