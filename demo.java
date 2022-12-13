70d5b06d2dfd4ecd5344d1e7d68de690aa598a07
Bhushan <bhushanlahore03@gmail.com>
2022-12-13 17:01:48 +0530

    reverse No added

--- demo.java
+++ demo.java
@@ -1,4 +1,18 @@
 class demo{
+
+
+	public static void reverseNo(int no){
+
+		int rev =0;
+		int n1 =no;
+		while(n1>0){
+			int temp = n1/10;
+			rev = (rev * 10) + rev;
+			n1 = n1%10;
+		}
+		
+		System.out.println(rev);
+	}
 public static void main(String args[]){
 
 System.out.println("HEELO WORLD");
